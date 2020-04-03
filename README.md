372e0d2b16d387ddd0e6c48b6bd3bfcca59e1df3
Tiemco de Jong <20199992+tdjong@users.noreply.github.com>
2020-04-03 22:18:57 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,5 @@
+# my-armbian-server
+
+* NextCloud via Docker on shared disk
+* MiniDLNA to share NextCloud disk to internal network
+
---
+++ nextcloud-with-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Based on https://docs.nextcloudpi.com/en/how-to-get-started-with-ncp-docker/
+HDD_MOUNT=/media/hdd
+NCDATA=${HDD_MOUNT}/ncdata
+if [ ! -d $NCDATA ]; then
+	mkdir $NCDATA
+fi
+
+# Initial Trusted Domain
+IFACE=$( ip r | grep "default via" | awk '{ print $5 }' )
+IP=$( ip a | grep "global $IFACE" | grep -oP '\d{1,3}(.\d{1,3}){3}' | head -1 )
+
+NAME=nextcloudpi
+
+docker stop $NAME
+docker rm $NAME
+docker run -d -p 443:443 -p 80:80 -p 4443:4443 --restart always -v ${NCDATA}:/data --name $NAME ownyourbits/nextcloudpi-armhf $IP
