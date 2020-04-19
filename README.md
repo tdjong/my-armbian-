a3ae3464fe4908dda9693f76867776db6dd5e928
Tiemco de Jong <20199992+tdjong@users.noreply.github.com>
2020-04-19 21:24:29 +0200

    NextCloudPi verbetering

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-# my-armbian-server
+# My armbian based data center
+
+## Base infrastructure
+
+* DNS server - for internal DNS entries
+* Traefik - convert docker container to DNS name with Lets Encrypt Certificate
+
+## User functionality
 
 * NextCloud via Docker on shared disk
-* MiniDLNA to share NextCloud disk to internal network
+* MiniDLNA to share NextCloud disk to internal network / TV
+* Gogs - internal Git repositories - Go based application in docker
 
+Inspired by https://github.com/kdeenkhoorn/docker-compose
+
+## External prerequisites
+
+* DNS entity controlled with API
--- nextcloud-with-docker.sh
+++ nextcloud-with-docker.sh
@@ -9,9 +9,15 @@ fi
 # Initial Trusted Domain
 IFACE=$( ip r | grep "default via" | awk '{ print $5 }' )
 IP=$( ip a | grep "global $IFACE" | grep -oP '\d{1,3}(.\d{1,3}){3}' | head -1 )
+echo "Using Trusted domain: $IP"
 
 NAME=nextcloudpi
 
+echo "Stopping and removing previous version"
 docker stop $NAME
 docker rm $NAME
-docker run -d -p 443:443 -p 80:80 -p 4443:4443 --restart always -v ${NCDATA}:/data --name $NAME ownyourbits/nextcloudpi-armhf $IP
+
+echo "Starting new container"
+docker run -d -p 443:443 -p 80:80 -p 4443:4443 \
+  --restart always -v ${NCDATA}:/data \
+  --name $NAME ownyourbits/nextcloudpi-armhf "$IP"
